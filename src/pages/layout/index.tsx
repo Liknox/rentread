7acdfb58077499a654e6b6103427e2742b16f4e5
Nazar Koval <n.kovaldev@gmail.com>
2025-01-25 22:52:08 +0200

    ref: wrap Layout component with AntLayout

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,16 +1,15 @@
+import { Layout as AntLayout } from "antd"
 import { createRootRoute, Outlet } from "@tanstack/react-router"
-import { TanStackRouterDevtools } from "@tanstack/router-devtools"
 import { Footer } from "widgets/footer"
 import { Header } from "widgets/header"
 
 const Layout = () => {
    return (
-      <div>
+      <AntLayout>
          <Header />
          <Outlet />
          <Footer />
-         <TanStackRouterDevtools />
-      </div>
+      </AntLayout>
    )
 }
 
