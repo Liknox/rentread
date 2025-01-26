292f6e2e12dfa33d6c2f5ecb5fc7f2aea75d68f7
Nazar Koval <n.kovaldev@gmail.com>
2025-01-26 03:04:50 +0200

    chore: create categories template data

---
+++ src/shared/api/fixtures/library/categories.ts
@@ -0,0 +1,36 @@
+type Category = {
+   id: number
+   name: string
+   slug: string
+   description: string
+   cover?: {
+      style: { color: string }
+   }
+}
+
+// FIXME: Later, instead of icons, use links to images.
+// FIXME: Add more specific works.
+
+export const IMAGINATIVE: Category = {
+   id: 1,
+   name: "Fiction literature",
+   slug: "fiction-literature",
+   description: "classic, fantasy, detective stories, screen adaptations",
+   cover: { style: { color: "#b0a894" } },
+}
+
+export const NOT_IMAGINATIVE: Category = {
+   id: 2,
+   name: "Non-fiction literature",
+   slug: "non-fiction-literature",
+   description: "art, self-improvement, health, psychology",
+   cover: { style: { color: "#94b0a3" } },
+}
+
+export const BUSINESS: Category = {
+   id: 3,
+   name: "Business literature",
+   slug: "business-literature",
+   description: "biographies, management, investments, marketing",
+   cover: { style: { color: "#94a7b0" } },
+}
