05be222e7132a630fe190c78a4494c65f8e2c99a
Nazar Koval <n.kovaldev@gmail.com>
2025-01-28 20:04:06 +0200

    fix: scss import in the main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client"
 import { App } from "./app"
 import "antd/dist/reset.css"
-import "./index.css"
+import "@app/styles/index.scss"
 
 const rootElement = document.getElementById("root")
 
