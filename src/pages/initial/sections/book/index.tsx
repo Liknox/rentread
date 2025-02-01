81bee90abe44b2d6dcab8c3990f789d0c344dcc5
Nazar Koval <n.kovaldev@gmail.com>
2025-02-01 16:23:59 +0200

    type: iteratable book element and other minor improvements

--- src/pages/initial/sections/book/index.tsx
+++ src/pages/initial/sections/book/index.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Col, Row } from "antd"
 import { BookCard } from "entities/book"
-import { fakeApi } from "shared/api"
+import { AbstractBook, fakeApi } from "shared/api"
 
 const Books = () => {
    const booksQuery = fakeApi.library.books.getPopular()
 
    return (
       <Row justify="space-between">
-         {booksQuery.map((book: any) => (
+         {booksQuery.map((book: AbstractBook) => (
             <Col key={book.id} className="px-5" span={6}>
                <BookCard data={book} size="small" className="h-full" />
             </Col>
--- src/shared/lib/string/index.ts
+++ src/shared/lib/string/index.ts
@@ -2,7 +2,7 @@ export function capitalize(term: string) {
    return term.charAt(0).toUpperCase() + term.slice(1)
 }
 
-export function textOverflow(term: string, limit: number = 100) {
+export function textOverflow(term: string, limit = 100) {
    if (term.length <= limit) return term
    return `${term.slice(0, limit)}...`
 }
