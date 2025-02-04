80d265d9c74f51ff0ea2697f40c92fd681ec1800
Nazar Koval <n.kovaldev@gmail.com>
2025-02-04 18:59:05 +0200

    lint: format files

--- src/pages/catalog/index.tsx
+++ src/pages/catalog/index.tsx
@@ -1,9 +1,11 @@
-import { Typography, Layout } from "antd"
+import { Layout, Typography } from "antd"
 
 function CatalogPage() {
    return (
       <Layout.Content>
-         <Typography.Title className="text-center mt-10" level={2}>Books Catalog</Typography.Title>
+         <Typography.Title className="text-center mt-10" level={2}>
+            Books Catalog
+         </Typography.Title>
          <Layout className="mt-16">
             {/* <Content /> */}
             {/* <Sidebar /> */}
