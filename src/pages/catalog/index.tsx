847a4850c79ee828a1d7ca4f70093ef44d34f95c
Nazar Koval <n.kovaldev@gmail.com>
2025-02-04 18:58:28 +0200

    chore: add styles

--- src/pages/catalog/index.tsx
+++ src/pages/catalog/index.tsx
@@ -2,9 +2,9 @@ import { Typography, Layout } from "antd"
 
 function CatalogPage() {
    return (
-      <Layout.Content className="">
-         <Typography.Title level={2}>Books Catalog</Typography.Title>
-         <Layout>
+      <Layout.Content>
+         <Typography.Title className="text-center mt-10" level={2}>Books Catalog</Typography.Title>
+         <Layout className="mt-16">
             {/* <Content /> */}
             {/* <Sidebar /> */}
          </Layout>
