6a6d3ef210a1f58945620fb9958ec5902459e0bc
Nazar Koval <n.kovaldev@gmail.com>
2025-02-08 12:03:50 +0200

    feat: create alert(error, success, warning, info) and api file

---
+++ src/shared/lib/alert/index.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { notification } from "antd"
+import { IconType } from "antd/es/notification/interface"
+import { ReactNode } from "react"
+
+/**
+ * NOTE: Moved to a separate module for:
+ * - Simplifying the API (more normalized and familiar)
+ * - Standardizing a unified placement for all alerts
+ */
+const generateOpener =
+   (type: IconType) =>
+   (
+      message: string,
+      description?: ReactNode,
+      icon?: ReactNode,
+      // eslint-disable-next-line max-params
+   ) => {
+      notification.open({ type, message, description, placement: "bottomRight", icon })
+   }
+
+const error = generateOpener("error")
+const success = generateOpener("success")
+const warn = generateOpener("warning")
+const info = generateOpener("info")
+
+const alert = { error, success, warn, info }
+
+export default alert
--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,3 +1,4 @@
 export * as dom from "./dom"
 export * as string from "./string"
 export * as browser from "./browser"
+export { default as alert } from "./alert"
