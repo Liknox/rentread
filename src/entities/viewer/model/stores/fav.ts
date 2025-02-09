5d7d1bb161e9191c8dc84a97a9e604c10acc81dc
Nazar Koval <n.kovaldev@gmail.com>
2025-02-09 15:43:19 +0200

    ref: browser .on reference

--- src/entities/viewer/model/stores/fav.ts
+++ src/entities/viewer/model/stores/fav.ts
@@ -7,13 +7,11 @@ import * as events from "../events"
 // export const initialState = fakeApi.viewer.getFavourite().map((it) => it.id);
 export const initialState: number[] = fakeApi.users.users.getViewer().favABooks
 
-export const $store = browser.createPersistStore(initialState, { name: "entities/viewer/fav" })
-
-console.log($store.getState())
-
-$store.on(events.toggleBook, (state, payload) => {
-   if (state.includes(payload)) {
-      return state.filter(it => it !== payload)
-   }
-   return [...state, payload]
-})
+export const $store = browser
+   .createPersistStore(initialState, { name: "entities/viewer/fav" })
+   .on(events.toggleBook, (state, payload) => {
+      if (state.includes(payload)) {
+         return state.filter(it => it !== payload)
+      }
+      return [...state, payload]
+   })
