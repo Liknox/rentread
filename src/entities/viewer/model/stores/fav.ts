2be0aebb43285eaf051e8332007ae76bb15e65a2
Nazar Koval <n.kovaldev@gmail.com>
2025-02-09 14:04:10 +0200

    feat: create store for favorites books

---
+++ src/entities/viewer/model/stores/fav.ts
@@ -0,0 +1,19 @@
+import { browser } from "shared/lib"
+// FIXME: temp! @hardcoded
+import { fakeApi } from "shared/api"
+import * as events from "../events"
+
+// FIXME: fetch later by API
+// export const initialState = fakeApi.viewer.getFavourite().map((it) => it.id);
+export const initialState: number[] = fakeApi.users.users.getViewer().favABooks
+
+export const $store = browser.createPersistStore(initialState, { name: "entities/viewer/fav" })
+
+console.log($store.getState())
+
+$store.on(events.toggleBook, (state, payload) => {
+   if (state.includes(payload)) {
+      return state.filter(it => it !== payload)
+   }
+   return [...state, payload]
+})
