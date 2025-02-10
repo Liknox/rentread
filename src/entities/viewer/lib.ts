8cc0ce92e475a49b2ca893ed4735c16bac442465
Nazar Koval <n.kovaldev@gmail.com>
2025-02-10 20:47:19 +0200

    feat: userNormalize func

---
+++ src/entities/viewer/lib.ts
@@ -0,0 +1,25 @@
+import { AbstractBook, Book, fakeApi, Order, Reservation, User } from "@shared/api"
+
+/* Tsss... Don't show this to anybody! */
+export const getUserNormalized = (user: User) => {
+   const opened: Order[] = fakeApi.checkout.orders.getByIds(user.openedOrders)
+
+   const openedBooks: Book[] = fakeApi.users.userBooks.getUserBooksByIds(opened.map(o => o.bookId))
+
+   const closed: Order[] = fakeApi.checkout.orders.getByIds(user.closedOrders)
+   const closedBooks: Book[] = fakeApi.users.userBooks.getUserBooksByIds(closed.map(o => o.bookId))
+   const closedPrices: number[] = closedBooks.map(cb => fakeApi.library.books.getPrice(cb.abstractBook))
+
+   const reserved: Reservation[] = fakeApi.checkout.reservations.getByIds(user.reservations)
+   const reservedBooks: AbstractBook[] = fakeApi.library.books.getByIds(reserved.map(o => o.aBookId))
+
+   return {
+      opened,
+      openedBooks,
+      closed,
+      closedBooks,
+      closedPrices,
+      reserved,
+      reservedBooks,
+   }
+}
