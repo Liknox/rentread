1f032e2c98244983f7d19f09492e21c5ee2d26c8
Nazar Koval <n.kovaldev@gmail.com>
2025-02-14 21:07:49 +0200

    ref: deprecated way of displaying anchor children

--- src/pages/profile/sidebar/index.tsx
+++ src/pages/profile/sidebar/index.tsx
@@ -4,11 +4,10 @@ import { topics } from "@app/configs/constants"
 export const Sidebar = () => {
    return (
       <Layout.Sider width={80}>
-         <Anchor className="mt-5 text-[12px]">
-            {topics.map(topic => (
-               <Anchor.Link key={topic.id} href={`#${topic.id}`} title={topic.title} />
-            ))}
-         </Anchor>
+         <Anchor
+            className="mt-5 text-[12px]"
+            items={topics.map(topic => ({ key: topic.id, title: topic.title, href: `#${topic.id}` }))}
+         />
       </Layout.Sider>
    )
 }
