854d5b85402e2aa4443a6287c700e0f14bb8a6bd
Nazar Koval <n.kovaldev@gmail.com>
2025-02-15 23:50:16 +0200

    feat: display book title

--- src/pages/book/index.tsx
+++ src/pages/book/index.tsx
@@ -2,8 +2,23 @@ import { useMatch } from "@tanstack/react-router"
 
 function BookPage() {
    const { params } = useMatch({ from: "/book/$bookId" }) as { params: { bookId: string } }
-   console.log(params?.bookId)
-   return <div>{params?.bookId}</div>
+
+   const bookId = Number(params?.bookId)
+   const book = fakeApi.library.books.getById(bookId)
+
+
+   return (
+      <Layout.Content className="mb-20">
+         <Link to="/catalog" className="text-primary">
+            Каталог
+         </Link>
+         <Typography.Title level={2}>{fakeApi.library.books.getBookString(book)}</Typography.Title>
+         <Row className="mt-8 mb-20">
+         </Row>
+         <Row>{/* <Recommendations book={book} /> */}</Row>
+      </Layout.Content>
+   )
+}
 }
 
 export default BookPage
