2284abc024f81af2c28d89b6fd5fc63bb98eaa0d
Nazar Koval <n.kovaldev@gmail.com>
2025-02-15 23:51:04 +0200

    chore: display content when book wasn't found

--- src/pages/book/index.tsx
+++ src/pages/book/index.tsx
@@ -1,4 +1,4 @@
-import { useMatch } from "@tanstack/react-router"
+import { Button, Carousel, Col, Descriptions, Layout, Result, Row, Typography } from "antd"
 
 function BookPage() {
    const { params } = useMatch({ from: "/book/$bookId" }) as { params: { bookId: string } }
@@ -6,6 +6,18 @@ function BookPage() {
    const bookId = Number(params?.bookId)
    const book = fakeApi.library.books.getById(bookId)
 
+   if (!book) {
+      return (
+         <Layout.Content className="flex justify-center items-center">
+            <Result
+               status="404"
+               title="404"
+               subTitle="Book not found"
+               extra={<Button href="/catalog">To the catalog</Button>}
+            />
+         </Layout.Content>
+      )
+   }
 
    return (
       <Layout.Content className="mb-20">
