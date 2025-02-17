7558ace1369797cf43c131687b9b6101a038bfbe
Nazar Koval <n.kovaldev@gmail.com>
2025-02-17 03:08:30 +0200

    chore: change button style if cart is empty

--- src/pages/order/cart/index.tsx
+++ src/pages/order/cart/index.tsx
@@ -1,5 +1,6 @@
 import { Link } from "@tanstack/react-router"
 import { Button, Layout } from "antd"
+import { orderModel } from "entities/order"
 import { Cart } from "features/cart"
 
 function Order() {
@@ -14,14 +15,21 @@ function Order() {
 }
 
 const Sidebar = () => {
+   const { isEmptyCart } = orderModel.cart.useOrderValidation()
 
    return (
       <Layout.Sider width={400}>
          <Cart.TotalInfo.Card>
             <Link to="/order/checkout">
+               {isEmptyCart ? (
+                  <Button block type="default" style={{ height: 50 }} disabled>
+                     Proceed to checkout
+                  </Button>
+               ) : (
                   <Button block type="primary" style={{ height: 50 }}>
                      Proceed to checkout
                   </Button>
+               )}
             </Link>
          </Cart.TotalInfo.Card>
       </Layout.Sider>
