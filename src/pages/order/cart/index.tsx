03a592f0f80285ccb3eb46800d0238b3837c8c34
Nazar Koval <n.kovaldev@gmail.com>
2025-02-17 20:37:08 +0200

    feat: display added books in content section on cart page

--- src/pages/order/cart/index.tsx
+++ src/pages/order/cart/index.tsx
@@ -1,5 +1,6 @@
 import { Link } from "@tanstack/react-router"
-import { Button, Layout } from "antd"
+import { Button, Col, Empty, Layout, Row, Typography } from "antd"
+import { BookRowCard } from "entities/book"
 import { orderModel } from "entities/order"
 import { Cart } from "features/cart"
 
@@ -8,9 +9,58 @@ function Order() {
       <Layout.Content>
          <Cart.Steps.View current={0} className="mb-10" />
          <Layout>
+            <Content />
             <Sidebar />
          </Layout>
       </Layout.Content>
+   )
+}
+
+const Content = () => {
+   const order = orderModel.cart.useOrder()
+   const durations = orderModel.cart.useOrderDurations()
+
+   return (
+      <Layout>
+         <Typography.Title level={2}>Cart</Typography.Title>
+         <section>
+            <Typography.Title level={3} type="secondary">
+               Order contents
+            </Typography.Title>
+            <Typography.Text className="block mb-5" type="secondary">
+               Check selected books before checkout
+            </Typography.Text>
+            <Row gutter={[0, 20]}>
+               {order.books.map(book => (
+                  <Col key={book.id} span={24}>
+                     <BookRowCard
+                        data={book}
+                        size="large"
+                        actions={
+                           <>
+                              <TariffRadio
+                                 onChange={value =>
+                                    orderModel.cart.events.setBookDuration({
+                                       bookId: book.id,
+                                       duration: value,
+                                    })
+                                 }
+                                 value={durations[book.id] || 14}
+                              />
+                           </>
+                        }
+                     />
+                  </Col>
+               ))}
+            </Row>
+            {!order.books.length && (
+               <Empty
+                  className="py-40 text-center text-lightslategray bg-[var(--color-accent)] rounded-[25px]"
+                  description="Order is empty"
+               />
+            )}
+         </section>
+      </Layout>
    )
 }
 
