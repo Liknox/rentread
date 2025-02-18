ce72d94d1db6bb373e530db1c01e519eb25b98cf
Nazar Koval <n.kovaldev@gmail.com>
2025-02-18 04:42:02 +0200

    chore: template sidebar component

--- src/pages/order/checkout/index.tsx
+++ src/pages/order/checkout/index.tsx
@@ -7,10 +7,35 @@ function Checkout() {
       <Layout.Content>
          <Cart.Steps.View current={1} className="mb-10" />
          <Layout>
+            <Sidebar />
          </Layout>
       </Layout.Content>
    )
 }
+const Sidebar = () => {
+   const validation = useCheckoutValidation()
+   return (
+      <Layout.Sider width={400}>
+         <Cart.TotalInfo.Card>
+            {validation.isTotallyAllowed ? (
+               <Button
+                  block
+                  type="primary"
+                  style={{ height: 50 }}
+                  title={validation.message}
+                  Pay for the order
+               </Button>
+            ) : (
+               <Button block type="default" style={{ height: 50 }} disabled>
+                  Pay for the order
+               </Button>
+            )}
+         </Cart.TotalInfo.Card>
+         {!validation.isEmptyCart && <Cart.TotalInfo.CartMini />}
+      </Layout.Sider>
+   )
+}
+
 }
 
 export default Checkout
