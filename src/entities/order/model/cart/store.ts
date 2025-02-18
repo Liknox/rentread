4b48be0fbd9dc5c42b900d842d31b39b05fb1443
Nazar Koval <n.kovaldev@gmail.com>
2025-02-18 05:04:11 +0200

    feat: create cart store and action on submitOrder event

--- src/entities/order/model/cart/store.ts
+++ src/entities/order/model/cart/store.ts
@@ -1,5 +1,9 @@
 import { browser } from "@shared/lib"
 import * as events from "./events"
+import { combine, sample } from "effector"
+import { fakeApi, Order } from "@shared/api"
+
+import { v4 as uuid } from "uuid"
 
 export const DEFAULT_DURATION = 14
 
@@ -59,3 +63,39 @@ export const $delivery = browser
       }
    })
 
+export const $cart = combine($books, $durations, (books, durations) => {
+   return { books, durations }
+})
+
+sample({
+   clock: events.submitOrder,
+   source: $cart,
+   fn: state => {
+      const viewer = fakeApi.users.users.getViewer()
+      const newOrders: Order[] = state.books.map(aBookId => {
+         return fakeApi.checkout.orders.createOrder({
+            bookId: fakeApi.users.userBooks.shuffleByABook(aBookId).id,
+            userId: viewer.id,
+            status: "WAITING_TRANSFER",
+            startDelta: 0,
+            deliveredDelta: 2,
+            endDelta: state.durations[aBookId] || 14,
+            costs: fakeApi.library.books.getPrice(fakeApi.library.books.getById(aBookId)!),
+         })
+      })
+
+      viewer.openedOrders.push(...newOrders.map(no => no.id))
+
+      fakeApi.checkout.orders.__pushTo(...newOrders)
+      fakeApi.users.users.__updateUser(viewer)
+
+      setTimeout(() => {
+         window.location.replace(`/order/result/${uuid()}`)
+      }, 0)
+
+      $books.reset()
+
+      return []
+   },
+   target: [$books, $durations],
+})
