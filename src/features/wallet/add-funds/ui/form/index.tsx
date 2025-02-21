1ce7a7a50dba003915cff10b7f9aba7cf86865cb
Nazar Koval <n.kovaldev@gmail.com>
2025-02-21 05:41:40 +0200

    feat: make unable to deposit more money than the upper range

--- src/features/wallet/add-funds/ui/form/index.tsx
+++ src/features/wallet/add-funds/ui/form/index.tsx
@@ -9,11 +9,12 @@ type Props = {
 }
 
 const MIN_MONEY = 5
+const MAX_MONEY = 100
 
 const AddFundsForm = ({ className }: Props) => {
    const viewer = viewerModel.useViewerWallet()
    const [money, setMoney] = useState(MIN_MONEY)
-   const isValid = money >= MIN_MONEY
+   const isValid = money >= MIN_MONEY && money <= MAX_MONEY
 
    return (
       <div className={cn("p-5", className)}>
@@ -21,7 +22,7 @@ const AddFundsForm = ({ className }: Props) => {
          <Form.Item
             // FIXME: simplify!
             validateStatus={isValid ? "success" : "error"}
-            help="The minimum payment is 5$">
+            help="The minimum payment is 5$, maximum 100$">
             <Input
                addonAfter="$"
                min={MIN_MONEY}
@@ -34,19 +35,24 @@ const AddFundsForm = ({ className }: Props) => {
          <Typography.Text type="secondary" className="block mt-10 mb-5">
             **The payment will be processed on an external service that complies with PCI DSS security standards.**
          </Typography.Text>
-         <Button
-            type="primary"
-            block
-            // href="#redirect-to-payment-service"
-            onClick={() => {
-               console.debug("[DEBUG] reachGoal: APPLY_TRANSACTION")
-               // FIXME: create wallet store and update this store (not only localStorage)
-               viewer.payment.applyTransaction(money).then(() => location.reload())
-            }}
-            loading={viewer.payment.isPending}
-            disabled={!isValid}>
-            Deposit
-         </Button>
+         {isValid ? (
+            <Button
+               type="primary"
+               block
+               // href="#redirect-to-payment-service"
+               onClick={() => {
+                  console.debug("[DEBUG] reachGoal: APPLY_TRANSACTION")
+                  // FIXME: create wallet store and update this store (not only localStorage)
+                  viewer.payment.applyTransaction(money).then(() => location.reload())
+               }}
+               loading={viewer.payment.isPending}>
+               Deposit
+            </Button>
+         ) : (
+            <Button block type="default" disabled>
+               Deposit
+            </Button>
+         )}
       </div>
    )
 }
