504368b86f9ea57769c79ce2cd421f4a061019ad
Nazar Koval <n.kovaldev@gmail.com>
2025-02-25 16:53:09 +0200

    chore: scroll to top on page change

--- src/pages/catalog/content/index.tsx
+++ src/pages/catalog/content/index.tsx
@@ -14,6 +14,7 @@ import { Fav } from "features/fav"
 import { Reserve } from "features/reserve"
 import { useState } from "react"
 import Sidebar from "../sidebar"
+import { scrollToTop } from "@shared/lib/dom"
 
 const { Option } = Select
 
@@ -80,6 +81,7 @@ function CatalogContent() {
    const [open, setOpen] = useState(false)
 
    const handlePageChange = (page: number, size: number) => {
+      scrollToTop()
       setCurrentPage(page)
       setPageSize(size)
    }
