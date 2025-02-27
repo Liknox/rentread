87445de040647146bd247e1dd81e46caa8f0882b
Nazar Koval <n.kovaldev@gmail.com>
2025-02-27 14:32:52 +0200

    chore: add change language buttons

--- src/widgets/footer/ui/index.tsx
+++ src/widgets/footer/ui/index.tsx
@@ -1,12 +1,17 @@
 import { Layout } from "antd"
+import { useTranslation } from "react-i18next"
 
 function Footer() {
+   const { i18n } = useTranslation()
+
    return (
       <Layout.Footer className="text-center">
          Rentread ©{new Date().getFullYear()} Created by{" "}
          <a href="https://github.com/Liknox/rentread" target="_blank" rel="noreferrer" className="text-primary">
             Nazar Koval
          </a>
+         <button onClick={() => i18n.changeLanguage("en")}>en</button>
+         <button onClick={() => i18n.changeLanguage("ua")}>ua</button>
       </Layout.Footer>
    )
 }
