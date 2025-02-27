f232a27f53dc640d4c7c41abd73e5816704d64ad
Nazar Koval <n.kovaldev@gmail.com>
2025-02-27 14:37:58 +0200

    chore: display test translation value

--- src/pages/initial/sections/ui/banner/index.tsx
+++ src/pages/initial/sections/ui/banner/index.tsx
@@ -2,14 +2,17 @@ import { isMobile } from "@shared/lib/browser"
 import { Link } from "@tanstack/react-router"
 import { Carousel, Typography } from "antd"
 import { bannerScreen } from "./model"
+import { useTranslation } from "react-i18next"
 
 const Banner = () => {
+   const { t } = useTranslation()
+
    return (
       <Carousel autoplay arrows={!isMobile} infinite autoplaySpeed={4000}>
          {bannerScreen.map(banner => (
             <div className="text-center relative h-80" key={banner.id}>
                <Typography.Text className="absolute z-10 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white w-[75%] sm:w-[50%]">
-                  <h3 className="!text-white">{banner.title}</h3>
+                  <h3 className="!text-white">{t(banner.title)}</h3>
                   <p className="w-[100%] m-[auto]">{banner.subtitle}</p>
                   <p className="hidden sm:block">{banner.description}</p>
                   {banner.link ? (
