168d5ce3087b76133485a3af7d32d7238f245bde
Nazar Koval <n.kovaldev@gmail.com>
2025-03-03 18:24:24 +0200

    chore: change view of footer

--- src/app/providers/locales/i18n.config.ts
+++ src/app/providers/locales/i18n.config.ts
@@ -5,7 +5,7 @@ import { initReactI18next } from "react-i18next"
 import enTranslations from "./en/translation.json"
 import uaTranslations from "./ua/translation.json"
 
-const savedLanguage = localStorage.getItem("i18nextLng") || "en"
+export const savedLanguage = localStorage.getItem("i18nextLng") || "en"
 
 i18n
    .use(LanguageDetector)
--- src/widgets/footer/ui/index.tsx
+++ src/widgets/footer/ui/index.tsx
@@ -1,25 +1,33 @@
 import { TRANSLATIONS } from "@app/configs/constants/translation"
+import { savedLanguage } from "@app/providers/locales/i18n.config"
 import { Layout, Select } from "antd"
 import { useTranslation } from "react-i18next"
 
 const { Option } = Select
 
+const langs = [
+   { value: "ua", label: "ua" },
+   { value: "en", label: "en" },
+]
+
 function Footer() {
    const { i18n, t } = useTranslation()
 
    return (
-      <Layout.Footer className="text-center">
-         Rentread ©{new Date().getFullYear()} {t(TRANSLATIONS.footer.text)}{" "}
-         <a href="https://github.com/Liknox/rentread" target="_blank" rel="noreferrer" className="text-primary">
-            {t(TRANSLATIONS.footer.name)}
-         </a>
-         <Select defaultValue="en" className="w-[60px] ml-2" onChange={value => i18n.changeLanguage(value)}>
-            <Option key={1} value="ua">
-               ua
-            </Option>
-            <Option key={2} value="en">
-               en
-            </Option>
+      <Layout.Footer className="flex justify-center items-center md:flex-row flex-col gap-1">
+         Rentread ©{new Date().getFullYear()}
+         <p>
+            {t(TRANSLATIONS.footer.text)}{" "}
+            <a href="https://github.com/Liknox/rentread" target="_blank" rel="noreferrer" className="text-primary">
+               {t(TRANSLATIONS.footer.name)}
+            </a>
+         </p>
+         <Select defaultValue={savedLanguage} className="w-[60px] ml-1" onChange={value => i18n.changeLanguage(value)}>
+            {langs.map(lang => (
+               <Option key={lang.value} value={lang.value}>
+                  {lang.label}
+               </Option>
+            ))}
          </Select>
       </Layout.Footer>
    )
