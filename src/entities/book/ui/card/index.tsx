7caf9dd7f0ee8cef05afa7cb03faf5200b5cd289
Nazar Koval <n.kovaldev@gmail.com>
2025-04-18 19:33:56 +0300

    ref: make BookCard component easier to manage

--- src/entities/book/ui/card/index.tsx
+++ src/entities/book/ui/card/index.tsx
@@ -3,11 +3,12 @@ import { Link } from "@tanstack/react-router"
 import { Card } from "antd"
 import cn from "classnames"
 import type { CSSProperties, ReactNode } from "react"
+import { memo } from "react"
 import { useTranslation } from "react-i18next"
 
 import { routes } from "@app/configs/constants"
 import { TRANSLATIONS } from "@app/configs/constants/translation"
-import { isMobile } from "@shared/lib/browser"
+import { useMobileDetection } from "@shared/lib/browser"
 
 import type { AbstractBook } from "shared/api"
 import { fakeApi } from "shared/api"
@@ -15,7 +16,7 @@ import { string } from "shared/lib"
 
 type Size = "default" | "small" | "mini"
 
-type Props = {
+type BookCardProps = {
    data: AbstractBook
    children?: ReactNode
    className?: string
@@ -38,21 +39,24 @@ const imgStyle: Record<Size, CSSProperties> = {
    mini: { padding: "40px 0", fontSize: 70 },
 }
 
-const BookCard = (props: Props) => {
+const BookCard = (props: BookCardProps) => {
    const { t } = useTranslation()
-   const { data: b, className, size = "default", children, actions, withPrice = true, asSecondary } = props
-   const author = b.authors.map(fakeApi.library.authors.getShortname).join(", ")
-   const publisher = `${b.publishingHouse.name}`
-   const title = `${author} — ${b.name}`
-   const description = `${publisher} (${b.publicationYear})`
+   const isMobileView = useMobileDetection()
+   const { data: book, className, size = "default", children, actions, withPrice = true, asSecondary } = props
+
+   const author = book.authors.map(fakeApi.library.authors.getShortname).join(", ")
+   const publisher = `${book.publishingHouse.name}`
+   const title = `${author} — ${book.name}`
+   const description = `${publisher} (${book.publicationYear})`
 
    const isDefault = size === "default"
    const isMini = size === "mini"
    const withDescription = props.withDescription || isDefault
+   const maxTitleLength = isMobileView ? 30 : 50
 
    return (
       <Card
-         key={b.id}
+         key={book.id}
          hoverable
          styles={{ body: bodyStyle[size] }}
          cover={<BookFilled style={imgStyle[size]} />}
@@ -70,15 +74,15 @@ const BookCard = (props: Props) => {
                <div className="flex flex-col">
                   {withPrice && (
                      <span className="font-semibold text-[20px]">
-                        {t(TRANSLATIONS.book.from)} {fakeApi.library.books.getPrice(b)} $
+                        {t(TRANSLATIONS.book.from)} {fakeApi.library.books.getPrice(book)} $
                      </span>
                   )}
-                  {isMini || (
+                  {!isMini && (
                      <Link
-                        to={`${routes.BOOK}/${b.id}`}
+                        to={`${routes.BOOK}/${book.id}`}
                         title={title}
                         className="text-blue-500 hover:underline whitespace-normal">
-                        {string.textOverflow(title, isMobile ? 30 : 50)}
+                        {string.textOverflow(title, maxTitleLength)}
                      </Link>
                   )}
                </div>
@@ -94,4 +98,4 @@ const BookCard = (props: Props) => {
    )
 }
 
-export default BookCard
+export default memo(BookCard)
