5b75fba0e9d4ca45eb81a28e3a15f9292a5c1f48
Nazar Koval <n.kovaldev@gmail.com>
2025-04-20 19:19:14 +0300

    style: make layout responsive

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -5,16 +5,18 @@ import { Footer } from "widgets/footer"
 import { Header } from "widgets/header"
 
 import { useResetScrollAtEveryPage } from "@pages/hooks"
+import { useMobileDetection } from "@shared/lib/browser"
 import { DemoAlert } from "@shared/ui/location-alert"
 
 const Layout = () => {
    useResetScrollAtEveryPage()
+   const isMobile = useMobileDetection()
 
    return (
-      <AntLayout>
+      <AntLayout className="min-h-screen flex flex-col">
          <DemoAlert />
          <Header />
-         <div className="mt-16 md:mt-0">
+         <div className={`flex-grow ${isMobile ? "mt-16 pt-2" : "mt-0"}`}>
             <Outlet />
          </div>
          <Footer />
