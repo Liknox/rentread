c366f421b332641a48b85243b2a40d1981e19af6
Nazar Koval <n.kovaldev@gmail.com>
2025-04-21 15:57:21 +0300

    ref: improve anchor sidebar ui on mobile screen

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from "react-i18next"
 
 import { TIMEOUT } from "@app/configs/constants"
 import { TRANSLATIONS } from "@app/configs/constants/translation"
+import { useBreakpoint } from "@shared/lib/browser"
 import { useTitle } from "@shared/lib/dom"
 import { Aside } from "entities/aside"
 import { Content } from "./content"
@@ -11,6 +12,7 @@ import { Sidebar } from "./sidebar"
 
 function ProfilePage() {
    const { t } = useTranslation()
+   const breakpoint = useBreakpoint()
    useTitle(t(TRANSLATIONS.pageTitle.profile))
 
    /* FIXME: Very bad practice, -_-) */
@@ -22,11 +24,18 @@ function ProfilePage() {
    }
 
    return (
-      <Layout.Content>
-         <Layout className="flex !flex-col md:!flex-row">
+      <Layout.Content className="p-4 md:p-[40px_10%] mb-20 md:mb-0">
+         <Layout className="flex !flex-col md:!flex-row bg-transparent">
             <Aside />
-            <Content />
-            <Sidebar forceUpdate={forceUpdate} />
+            <div className="flex-grow">
+               <Content />
+            </div>
+            {!breakpoint.xs && <Sidebar forceUpdate={forceUpdate} className="ml-0 md:ml-6 mt-6 md:mt-0" />}
+            {breakpoint.xs && (
+               <div className="fixed bottom-2 left-0 right-0 backdrop-blur-sm shadow-lg z-10 p-2">
+                  <Sidebar forceUpdate={forceUpdate} isMobileView={true} />
+               </div>
+            )}
          </Layout>
       </Layout.Content>
    )
--- src/pages/profile/sidebar/index.tsx
+++ src/pages/profile/sidebar/index.tsx
@@ -1,16 +1,39 @@
-import { Anchor, Layout } from "antd"
+import { Anchor, Layout, Space, Button } from "antd"
 import { useTranslation } from "react-i18next"
 
 import { topics } from "@app/configs/constants"
+import { useMobileDetection } from "@shared/lib/browser"
 
 type Props = {
    forceUpdate: () => void
+   className?: string
+   isMobileView?: boolean
 }
 
 export const Sidebar = (props: Props) => {
    const { t } = useTranslation()
+   const isMobile = useMobileDetection()
+   const { className = "", isMobileView = false } = props
+
+   if (isMobileView) {
+      return (
+         <Space className="w-full flex justify-around">
+            {topics.map(topic => (
+               <Button
+                  key={topic.id}
+                  type="text"
+                  href={`#${topic.id}`}
+                  onClick={props.forceUpdate}
+                  className="text-xs py-1 px-2">
+                  {t(topic.title)}
+               </Button>
+            ))}
+         </Space>
+      )
+   }
+
    return (
-      <Layout.Sider width={80} className="md:block hidden">
+      <Layout.Sider width={80} className={`md:block ${isMobile ? "hidden" : ""} ${className}`}>
          <Anchor
             className="mt-5 text-[12px]"
             items={topics.map(topic => ({ key: topic.id, title: t(topic.title), href: `#${topic.id}` }))}
