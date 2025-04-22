aa6a97ce903c45c4e4fb371ca16164761f63452f
Nazar Koval <n.kovaldev@gmail.com>
2025-04-22 13:51:57 +0300

    feat: create LoadingState class for managing skeleton loading

---
+++ src/shared/lib/loadingState.ts
@@ -0,0 +1,23 @@
+class LoadingState {
+   private static instance: LoadingState
+   private loadedSections: Record<string, boolean> = {}
+
+   private constructor() {}
+
+   public static getInstance(): LoadingState {
+      if (!LoadingState.instance) {
+         LoadingState.instance = new LoadingState()
+      }
+      return LoadingState.instance
+   }
+
+   public hasLoaded(section: string): boolean {
+      return this.loadedSections[section] || false
+   }
+
+   public markAsLoaded(section: string): void {
+      this.loadedSections[section] = true
+   }
+}
+
+export const loadingState = LoadingState.getInstance()
